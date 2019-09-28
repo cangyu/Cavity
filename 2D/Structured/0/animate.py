82dc556af434434c9faf06c2601c7ebb73773749
Yu Cang <yhcy1993@gmail.com>
2019-09-28 18:30:40 +0800

    add convergence history plot

---
+++ 2D/Structured/0/animate.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = np.loadtxt('history.txt')
+
+t = data[:, 0]
+div = np.log(np.abs(data[:, 1]))
+
+plt.plot(t, div, '-')
+plt.xlabel('t/s')
+plt.ylabel(r'$log(|\nabla \vec{U}|)$')
+plt.show()
