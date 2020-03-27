40a7e9221ed9d74f082f9630d437a526c3c512cc
Yu Cang <yhcy1993@gmail.com>
2020-03-27 17:58:42 +0800

    add handling of Dirichlet Boundary(0-value assumed) for dp.

--- 3D/Incompressible/Laminar/FSM/Explicit/src/PoissonEqn.cc
+++ 3D/Incompressible/Laminar/FSM/Explicit/src/PoissonEqn.cc
@@ -20,10 +20,10 @@ void calcPressureCorrectionEquationCoef(Eigen::SparseMatrix<Scalar> &A)
 {
     std::list<Eigen::Triplet<Scalar>> coef;
 
-    // Original coefficients
+    /// Step1: Calculate original coefficients for each cell.
     for (const auto &C : cell)
     {
-        // Initialize coefficient baseline.
+        /// Initialize coefficient baseline.
         std::map<int, Scalar> cur_coef;
         cur_coef[C.index] = 0.0;
         for (auto F : C.adjCell)
@@ -39,19 +39,32 @@ void calcPressureCorrectionEquationCoef(Eigen::SparseMatrix<Scalar> &A)
             }
         }
 
-        // Compute coefficient contributions.
+        /// Compute coefficient contributions.
         const auto N_C = C.surface.size();
         for (int f = 1; f <= N_C; ++f)
         {
             const auto &S_f = C.S(f);
             auto curFace = C.surface(f);
 
-            if (curFace->atBdry)
+            if (curFace->atBdry) /// Boundary Case.
             {
-                // No need to handle boundary faces as zero-gradient B.C. is assumed for dp.
-                continue;
+                if (curFace->p_BC == Dirichlet)
+                {
+                    /// When p is given on boundary, dp is 0-value there.
+                    const Vector r_C = curFace->center - C.center;
+                    const Vector &S_f = C.S(f);
+                    cur_coef[C.index] -= r_C.dot(S_f) / r_C.dot(r_C);
+                }
+                else if (curFace->p_BC == Neumann)
+                {
+                    /// When p is not determined on boundary, dp is 0-gradient
+                    /// there, thus contribution is 0 and no need to handle.
+                    continue;
+                }
+                else
+                    throw unsupported_boundary_condition(curFace->p_BC);
             }
-            else
+            else /// Internal Case.
             {
                 auto F = C.adjCell(f);
                 if (!F)
@@ -70,58 +83,86 @@ void calcPressureCorrectionEquationCoef(Eigen::SparseMatrix<Scalar> &A)
                 const Eigen::VectorXd J_C = ksi_f * y_f.transpose() * C.J_INV_p;
                 const Eigen::VectorXd J_F = (1.0 - ksi_f) * y_f.transpose() * F->J_INV_p;
 
-                // Part1
+                /// Part1
                 const auto p1_coef = x_f / d_f;
                 cur_coef[F->index] += p1_coef;
                 cur_coef[C.index] -= p1_coef;
 
-                // Part2
+                /// Part2
                 for (int i = 0; i < N_C; ++i)
                 {
                     auto C_i = C.adjCell.at(i);
-                    if (C_i)
+                    const auto p2_coef = J_C(i);
+
+                    if (C_i) /// Adjacent cell exists.
                     {
-                        const auto p2_coef = J_C(i);
                         cur_coef[C_i->index] += p2_coef;
                         cur_coef[C.index] -= p2_coef;
                     }
-                    else
+                    else /// Adjacent cell does not exist.
                     {
-                        // No need to handle boundary faces as zero-gradient B.C. is assumed for dp.
-                        continue;
+                        const auto localFace = C.surface.at(i);
+
+                        if (localFace->p_BC == Dirichlet)
+                        {
+                            /// When p is given on boundary, dp is 0-value there.
+                            cur_coef[C.index] -= p2_coef;
+                        }
+                        else if (localFace->p_BC == Neumann)
+                        {
+                            /// When p is not determined on boundary, dp is 0-gradient
+                            /// there, thus contribution of this line is 0.
+                            continue;
+                        }
+                        else
+                            throw unsupported_boundary_condition(localFace->p_BC);
                     }
                 }
 
-                // Part3
+                /// Part3
                 for (auto i = 0; i < N_F; ++i)
                 {
                     auto F_i = F->adjCell.at(i);
-                    if (F_i)
+                    const auto p3_coef = J_F(i);
+
+                    if (F_i) /// Adjacent cell exists.
                     {
-                        const auto p3_coef = J_F(i);
                         cur_coef[F_i->index] += p3_coef;
                         cur_coef[F->index] -= p3_coef;
                     }
-                    else
+                    else /// Adjacent cell does not exist.
                     {
-                        // No need to handle boundary faces as zero-gradient B.C. is assumed for dp.
-                        continue;
+                        const auto localFace = F->surface.at(i);
+
+                        if (localFace->p_BC == Dirichlet)
+                        {
+                            /// When p is given on boundary, dp is 0-value there.
+                            cur_coef[F->index] -= p3_coef;
+                        }
+                        else if (localFace->p_BC == Neumann)
+                        {
+                            /// When p is not determined on boundary, dp is 0-gradient
+                            /// there, thus contribution of this line is 0.
+                            continue;
+                        }
+                        else
+                            throw unsupported_boundary_condition(localFace->p_BC);
                     }
                 }
             }
         }
 
-        // Record current line.
-        // Convert index to 0-based.
+        /// Record current line.
+        /// Convert index to 0-based.
         for (auto it = cur_coef.begin(); it != cur_coef.end(); ++it)
             coef.emplace_back(C.index - 1, it->first - 1, it->second);
     }
 
-    // Set reference
+    /// Step2: Set reference.
     coef.remove_if(is_ref_row);
     coef.emplace_back(0, 0, 1.0);
 
-    // Assemble
+    /// Step3: Assemble.
     A.setFromTriplets(coef.begin(), coef.end());
 }
 
@@ -141,6 +182,6 @@ void calcPressureCorrectionEquationRHS(Eigen::Matrix<Scalar, Eigen::Dynamic, 1> 
         }
     }
 
-    // Set reference
+    /// Set reference
     rhs[ref_cell] = ref_val;
 }
