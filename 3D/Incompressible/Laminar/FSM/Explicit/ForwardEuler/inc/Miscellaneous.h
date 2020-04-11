a84fbd5616d4d2e51a792dd10162f79750618f07
Yu Cang <yhcy1993@gmail.com>
2020-04-11 15:36:56 +0800

    add run tag

---
+++ 3D/Incompressible/Laminar/FSM/Explicit/ForwardEuler/inc/Miscellaneous.h
@@ -0,0 +1,11 @@
+#ifndef __MISCELLANEOUS_H__
+#define __MISCELLANEOUS_H__
+
+#include <ctime>
+#include <string>
+
+double duration(const clock_t &startTime, const clock_t &endTime);
+std::string time_stamp_str();
+
+
+#endif // !__MISCELLANEOUS_H__
---
+++ 3D/Incompressible/Laminar/FSM/Explicit/ForwardEuler/src/Miscellaneous.cc
@@ -0,0 +1,17 @@
+#include "../inc/Miscellaneous.h"
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+double duration(const clock_t &startTime, const clock_t &endTime)
+{
+    return (endTime - startTime) * 1.0 / CLOCKS_PER_SEC;
+}
+
+std::string time_stamp_str()
+{
+    auto tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::ostringstream ss;
+    ss << std::put_time(std::localtime(&tt), "%Y%m%d%H%M%S");
+    return ss.str();
+}
