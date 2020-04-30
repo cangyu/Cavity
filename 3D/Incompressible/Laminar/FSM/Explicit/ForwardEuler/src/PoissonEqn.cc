f2b1f6ab39093f2bd84ce04130fa1df92251320f
Yu Cang <yhcy1993@gmail.com>
2020-04-30 14:55:25 +0800

    add coo to csr conversion

--- 3D/Incompressible/Laminar/FSM/Explicit/ForwardEuler/src/PoissonEqn.cc
+++ 3D/Incompressible/Laminar/FSM/Explicit/ForwardEuler/src/PoissonEqn.cc
@@ -11,16 +11,14 @@ extern NaturalArray<Patch> patch;
 static const size_t ref_cell = 0; /// 0-based
 static const Scalar ref_val = 0.0; /// Pa
 
-bool is_ref_row(const Eigen::Triplet<Scalar> &x)
+static bool is_ref_row(const Eigen::Triplet<Scalar> &x)
 {
     return x.row() == ref_cell;
 }
 
-void calcPressureCorrectionEquationCoef(Eigen::SparseMatrix<Scalar> &A)
+static void gen_coef_triplets(std::list<Eigen::Triplet<Scalar>> &coef)
 {
-    std::list<Eigen::Triplet<Scalar>> coef;
-
-    /// Step1: Calculate original coefficients for each cell.
+    /// Calculate original coefficients for each cell.
     for (const auto &C : cell)
     {
         /// Initialize coefficient baseline.
@@ -158,11 +156,19 @@ void calcPressureCorrectionEquationCoef(Eigen::SparseMatrix<Scalar> &A)
             coef.emplace_back(C.index - 1, it->first - 1, it->second);
     }
 
-    /// Step2: Set reference.
+    /// Set reference.
     coef.remove_if(is_ref_row);
     coef.emplace_back(0, 0, 1.0);
+}
 
-    /// Step3: Assemble.
+void calcPressureCorrectionEquationCoef(Eigen::SparseMatrix<Scalar> &A)
+{
+    std::list<Eigen::Triplet<Scalar>> coef;
+
+    /// Calculate raw triplets
+    gen_coef_triplets(coef);
+
+    /// Assemble.
     A.setFromTriplets(coef.begin(), coef.end());
 }
 
@@ -184,4 +190,101 @@ void calcPressureCorrectionEquationRHS(Eigen::Matrix<Scalar, Eigen::Dynamic, 1> 
 
     /// Set reference
     rhs[ref_cell] = ref_val;
+}
+
+/// Copy from SciPy V1.4.1
+/// https://github.com/scipy/scipy/blob/v1.4.1/scipy/sparse/sparsetools/coo.h
+template <class I, class T>
+void coo_tocsr(const I n_row,
+               const I n_col,
+               const I nnz,
+               const I Ai[],
+               const I Aj[],
+               const T Ax[],
+                     I Bp[],
+                     I Bj[],
+                     T Bx[])
+{
+    //compute number of non-zero entries per row of A 
+    std::fill(Bp, Bp + n_row, 0);
+
+    for (I n = 0; n < nnz; n++){            
+        Bp[Ai[n]]++;
+    }
+
+    //cumsum the nnz per row to get Bp[]
+    for(I i = 0, cumsum = 0; i < n_row; i++){     
+        I temp = Bp[i];
+        Bp[i] = cumsum;
+        cumsum += temp;
+    }
+    Bp[n_row] = nnz; 
+
+    //write Aj,Ax into Bj,Bx
+    for(I n = 0; n < nnz; n++){
+        I row  = Ai[n];
+        I dest = Bp[row];
+
+        Bj[dest] = Aj[n];
+        Bx[dest] = Ax[n];
+
+        Bp[row]++;
+    }
+
+    for(I i = 0, last = 0; i <= n_row; i++){
+        I temp = Bp[i];
+        Bp[i]  = last;
+        last   = temp;
+    }
+
+    //now Bp,Bj,Bx form a CSR representation (with possible duplicates)
+}
+
+void calcPressureCorrectionEquationCoef(SX_MAT &B)
+{
+    /// Calculate raw triplets
+    std::list<Eigen::Triplet<Scalar>> coef;
+
+    gen_coef_triplets(coef);
+
+    /// Set size
+    B.num_rows = B.num_cols = cell.size();
+    B.num_nnzs = coef.size();
+
+    /// Transform
+    auto Ai = new SX_INT[B.num_nnzs];
+    auto Aj = new SX_INT[B.num_nnzs];
+    auto Ax = new SX_FLT[B.num_nnzs];
+
+    int n = 0;
+    for(const auto &e : coef)
+    {
+        Ai[n] = e.row();
+        Aj[n] = e.col();
+        Ax[n] = e.value();
+        ++n;
+    }
+
+    /// Allocate storage
+    B.Ap = (SX_INT *)sx_malloc(sizeof(SX_INT) * (B.num_rows + 1));
+    B.Aj = (SX_INT *)sx_malloc(sizeof(SX_INT) * B.num_nnzs);
+    B.Ax = (SX_FLT *)sx_malloc(sizeof(SX_FLT) * B.num_nnzs);
+
+    /// COO to CSR
+    coo_tocsr<SX_INT, SX_FLT>(B.num_rows, B.num_cols, B.num_nnzs, Ai, Aj, Ax, B.Ap, B.Aj, B.Ax);
+
+    /// Release
+    delete[] Ai;
+    delete[] Aj;
+    delete[] Ax;
+}
+
+void calcPressureCorrectionEquationRHS(SX_VEC &rhs)
+{
+    
+}
+
+void prepare_dp_solver()
+{
+    
 }
