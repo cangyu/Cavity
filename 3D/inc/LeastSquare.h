a70d84cae1db64eb1e4289e8ca5f1d85fbec0942
Yu Cang <yhcy1993@gmail.com>
2020-07-16 15:06:05 +0800

    Add back J_INV matrix for each primitive variable; Fix values used on Neumann boundary, using surface unit outward normal vector instead of displacement vector.

--- 3D/inc/LeastSquare.h
+++ 3D/inc/LeastSquare.h
@@ -1,6 +1,6 @@
 #ifndef LEAST_SQUARE_H
 #define LEAST_SQUARE_H
 
-void calcLeastSquareCoef();
+void calc_least_square_coefficient_matrix();
 
 #endif
--- 3D/src/LeastSquare.cc
+++ 3D/src/LeastSquare.cc
@@ -1,7 +1,9 @@
 #include "../inc/custom_type.h"
 #include "../inc/LeastSquare.h"
 
-extern int NumOfPnt, NumOfFace, NumOfCell;
+extern int NumOfPnt;
+extern int NumOfFace;
+extern int NumOfCell;
 extern NaturalArray<Point> pnt;
 extern NaturalArray<Face> face;
 extern NaturalArray<Cell> cell;
@@ -12,7 +14,7 @@ extern NaturalArray<Patch> patch;
  * @param J The coefficient matrix to be factorized.
  * @param J_INV The general inverse of input matrix using QR decomposition.
  */
-static void extractQRMatrix
+static void extract_qr_matrix
 (
     const Eigen::Matrix<Scalar, Eigen::Dynamic, 3> &J, 
     Eigen::Matrix<Scalar, 3, Eigen::Dynamic> &J_INV
@@ -32,43 +34,190 @@ static void extractQRMatrix
 
 /**
  * QR decomposition matrix of each cell.
- * Ghost cells are used when the B.C. of boundary faces are set to Neumann type.
  */
-void calcLeastSquareCoef()
+void calc_least_square_coefficient_matrix()
 {
     typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 3> Mat;
 
-    Mat J;
+    Mat J_rho;
+    std::array<Mat, 3> J_U;
+    Mat J_p, J_p_prime;
+    Mat J_T;
 
     for (auto &c : cell)
     {
         const auto nF = c.surface.size();
 
-        J.resize(nF, Eigen::NoChange);
+        J_rho.resize(nF, Eigen::NoChange);
+        J_U[0].resize(nF, Eigen::NoChange);
+        J_U[1].resize(nF, Eigen::NoChange);
+        J_U[2].resize(nF, Eigen::NoChange);
+        J_p.resize(nF, Eigen::NoChange);
+        J_p_prime.resize(nF, Eigen::NoChange);
+        J_T.resize(nF, Eigen::NoChange);
 
         for (int j = 0; j < nF; ++j)
         {
+            /// Possible coefficients for current face
             auto curFace = c.surface.at(j);
-
-            Scalar dx, dy, dz;
+            Vector d, n;
             if (curFace->atBdry)
             {
-                dx = curFace->center.x() - c.center.x();
-                dy = curFace->center.y() - c.center.y();
-                dz = curFace->center.z() - c.center.z();
+                d = curFace->center - c.center;
+                n = c.S.at(j) / curFace->area;
+
+                /// Density
+                switch (curFace->parent->rho_BC)
+                {
+                case Dirichlet:
+                    J_rho.row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_rho.row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
+
+                /// Velocity-X
+                switch (curFace->parent->U_BC[0])
+                {
+                case Dirichlet:
+                    J_U[0].row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_U[0].row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
+
+                /// Velocity-Y
+                switch (curFace->parent->U_BC[1])
+                {
+                case Dirichlet:
+                    J_U[1].row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_U[1].row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
+
+                /// Velocity-Z
+                switch (curFace->parent->U_BC[2])
+                {
+                case Dirichlet:
+                    J_U[2].row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_U[2].row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
+
+                /// Pressure
+                switch (curFace->parent->p_BC)
+                {
+                case Dirichlet:
+                    J_p.row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_p.row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
+
+                /// Pressure-Correction
+                switch (curFace->parent->p_prime_BC)
+                {
+                case Dirichlet:
+                    J_p_prime.row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_p_prime.row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
+
+                /// Temperature
+                switch (curFace->parent->T_BC)
+                {
+                case Dirichlet:
+                    J_T.row(j) << d.x(), d.y(), d.z();
+                    break;
+                case Neumann:
+                    J_T.row(j) << n.x(), n.y(), n.z();
+                    break;
+                case Robin:
+                    throw robin_bc_is_not_supported();
+                default:
+                    break;
+                }
             }
             else
             {
                 auto curAdjCell = c.adjCell.at(j);
+                d = curAdjCell->center - c.center;
 
-                dx = curAdjCell->center.x() - c.center.x();
-                dy = curAdjCell->center.y() - c.center.y();
-                dz = curAdjCell->center.z() - c.center.z();
+                /// Density
+                J_rho.row(j) << d.x(), d.y(), d.z();
+
+                /// Velocity-X
+                J_U[0].row(j) << d.x(), d.y(), d.z();
+
+                /// Velocity-Y
+                J_U[1].row(j) << d.x(), d.y(), d.z();
+
+                /// Velocity-Z
+                J_U[2].row(j) << d.x(), d.y(), d.z();
+
+                /// Pressure
+                J_p.row(j) << d.x(), d.y(), d.z();
+
+                /// Pressure-Correction
+                J_p_prime.row(j) << d.x(), d.y(), d.z();
+
+                /// Temperature
+                J_T.row(j) << d.x(), d.y(), d.z();
             }
-
-            J.row(j) << dx, dy, dz;
         }
 
-        extractQRMatrix(J, c.J_INV);
+        /// Density
+        extract_qr_matrix(J_rho, c.J_INV_rho);
+
+        /// Velocity-X
+        extract_qr_matrix(J_U[0], c.J_INV_U[0]);
+
+        /// Velocity-Y
+        extract_qr_matrix(J_U[1], c.J_INV_U[1]);
+
+        /// Velocity-Z
+        extract_qr_matrix(J_U[2], c.J_INV_U[2]);
+
+        /// Pressure
+        extract_qr_matrix(J_p, c.J_INV_p);
+
+        /// Pressure-Correction
+        extract_qr_matrix(J_p_prime, c.J_INV_p_prime);
+
+        /// Temperature
+        extract_qr_matrix(J_T, c.J_INV_T);
     }
 }
