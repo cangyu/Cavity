368966ff0bee87f8be82dd8c75609a788bd1c579
Yu Cang <yhcy1993@gmail.com>
2020-07-16 15:12:04 +0800

    Update interpolation of face values.

--- 3D/src/Discretization.cc
+++ 3D/src/Discretization.cc
@@ -2,12 +2,13 @@
 #include "../inc/PoissonEqn.h"
 #include "../inc/CHEM.h"
 #include "../inc/IC.h"
-#include "../inc/LeastSquare.h"
 #include "../inc/Gradient.h"
 #include "../inc/Flux.h"
 #include "../inc/Discretization.h"
 
-extern int NumOfPnt, NumOfFace, NumOfCell;
+extern int NumOfPnt;
+extern int NumOfFace;
+extern int NumOfCell;
 extern NaturalArray<Point> pnt;
 extern NaturalArray<Face> face;
 extern NaturalArray<Cell> cell;
@@ -43,6 +44,129 @@ void calcFaceProperty()
 
 /*********************************************** Spatial Discretization ***********************************************/
 
+static void calcBoundaryFacePrimitiveValue(Face &f, Cell *c, const Vector &d)
+{
+    auto p = f.parent;
+
+    /// density
+    switch (p->rho_BC)
+    {
+    case Dirichlet:
+        break;
+    case Neumann:
+        f.rho = c->rho + f.grad_rho.dot(d);
+        break;
+    case Robin:
+        throw robin_bc_is_not_supported();
+    default:
+        break;
+    }
+
+    /// velocity-x
+    switch (p->U_BC[0])
+    {
+    case Dirichlet:
+        break;
+    case Neumann:
+        f.U.x() = c->U.x() + f.grad_U.col(0).dot(d);
+        break;
+    case Robin:
+        throw robin_bc_is_not_supported();
+    default:
+        break;
+    }
+
+    /// velocity-y
+    switch (p->U_BC[1])
+    {
+    case Dirichlet:
+        break;
+    case Neumann:
+        f.U.y() = c->U.y() + f.grad_U.col(1).dot(d);
+        break;
+    case Robin:
+        throw robin_bc_is_not_supported();
+    default:
+        break;
+    }
+
+    /// velocity-z
+    switch (p->U_BC[2])
+    {
+    case Dirichlet:
+        break;
+    case Neumann:
+        f.U.z() = c->U.z() + f.grad_U.col(2).dot(d);
+        break;
+    case Robin:
+        throw robin_bc_is_not_supported();
+    default:
+        break;
+    }
+
+    /// pressure
+    switch (p->p_BC)
+    {
+    case Dirichlet:
+        break;
+    case Neumann:
+        f.p = c->p + f.grad_p.dot(d);
+        break;
+    case Robin:
+        throw robin_bc_is_not_supported();
+    default:
+        break;
+    }
+
+    /// temperature
+    switch (p->T_BC)
+    {
+    case Dirichlet:
+        break;
+    case Neumann:
+        f.T = c->T + f.grad_T.dot(d);
+        break;
+    case Robin:
+        throw robin_bc_is_not_supported();
+    default:
+        break;
+    }
+}
+
+static void calcInternalFacePrimitiveValue(Face &f)
+{
+    /// pressure
+    const Scalar p_0 = f.c0->p + f.c0->grad_p.dot(f.r0);
+    const Scalar p_1 = f.c1->p + f.c1->grad_p.dot(f.r1);
+    f.p = 0.5 * (p_0 + p_1);
+
+    /// temperature
+    const Scalar T_0 = f.c0->T + f.c0->grad_T.dot(f.r0);
+    const Scalar T_1 = f.c1->T + f.c1->grad_T.dot(f.r1);
+    f.T = f.ksi0 * T_0 + f.ksi1 * T_1;
+
+    /// velocity
+    if (f.U.dot(f.n01) > 0)
+    {
+        const Scalar u_0 = f.c0->U.x() + f.c0->grad_U.col(0).dot(f.r0);
+        const Scalar v_0 = f.c0->U.y() + f.c0->grad_U.col(1).dot(f.r0);
+        const Scalar w_0 = f.c0->U.z() + f.c0->grad_U.col(2).dot(f.r0);
+        f.U = {u_0, v_0, w_0};
+    } else
+    {
+        const Scalar u_1 = f.c1->U.x() + f.c1->grad_U.col(0).dot(f.r1);
+        const Scalar v_1 = f.c1->U.y() + f.c1->grad_U.col(1).dot(f.r1);
+        const Scalar w_1 = f.c1->U.z() + f.c1->grad_U.col(2).dot(f.r1);
+        f.U = {u_1, v_1, w_1};
+    }
+
+    /// density
+    if (f.U.dot(f.n01) > 0)
+        f.rho = f.c0->rho + f.c0->grad_rho.dot(f.r0);
+    else
+        f.rho = f.c1->rho + f.c1->grad_rho.dot(f.r1);
+}
+
 void calcFaceValue()
 {
     for (auto &f : face)
@@ -51,214 +175,13 @@ void calcFaceValue()
         if (f.atBdry)
         {
             if (f.c0)
-            {
-                /// density
-                switch (f.rho_BC)
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.rho = f.c0->rho + (f.sn_grad_rho * f.n01 + f.c0->grad_rho).dot(f.r0) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// velocity-x
-                switch (f.U_BC[0])
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.U.x() = f.c0->U.x() + (f.sn_grad_U.x() * f.n01 + f.c0->grad_U.col(0)).dot(f.r0) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// velocity-y
-                switch (f.U_BC[1])
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.U.y() = f.c0->U.y() + (f.sn_grad_U.y() * f.n01 + f.c0->grad_U.col(1)).dot(f.r0) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// velocity-z
-                switch (f.U_BC[2])
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.U.z() = f.c0->U.z() + (f.sn_grad_U.z() * f.n01 + f.c0->grad_U.col(2)).dot(f.r0) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// pressure
-                switch (f.p_BC)
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.p = f.c0->p + (f.sn_grad_p * f.n01 + f.c0->grad_p).dot(f.r0) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// temperature
-                switch (f.T_BC)
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.T = f.c0->T + (f.sn_grad_T * f.n01 + f.c0->grad_T).dot(f.r0) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-            }
+                calcBoundaryFacePrimitiveValue(f, f.c0, f.r0);
             else if (f.c1)
-            {
-                /// density
-                switch (f.rho_BC)
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.rho = f.c1->rho + (f.sn_grad_rho * f.n10 + f.c1->grad_rho).dot(f.r1) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// velocity-x
-                switch (f.U_BC[0])
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.U.x() = f.c1->U.x() + (f.sn_grad_U.x() * f.n10 + f.c1->grad_U.col(0)).dot(f.r1) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// velocity-y
-                switch (f.U_BC[1])
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.U.y() = f.c1->U.y() + (f.sn_grad_U.y() * f.n10 + f.c1->grad_U.col(1)).dot(f.r1) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// velocity-z
-                switch (f.U_BC[2])
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.U.z() = f.c1->U.z() + (f.sn_grad_U.z() * f.n10 + f.c1->grad_U.col(2)).dot(f.r1) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// pressure
-                switch (f.p_BC)
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.p = f.c1->p + (f.sn_grad_p * f.n10 + f.c1->grad_p).dot(f.r1) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-
-                /// temperature
-                switch (f.T_BC)
-                {
-                case Dirichlet:
-                    break;
-                case Neumann:
-                    f.T = f.c1->T + (f.sn_grad_T * f.n10 + f.c1->grad_T).dot(f.r1) / 2;
-                    break;
-                case Robin:
-                    throw robin_bc_is_not_supported();
-                default:
-                    break;
-                }
-            }
+                calcBoundaryFacePrimitiveValue(f, f.c1, f.r1);
             else
                 throw empty_connectivity(f.index);
-        }
-        else
-        {
-            /// pressure
-            const Scalar p_0 = f.c0->p + f.c0->grad_p.dot(f.r0);
-            const Scalar p_1 = f.c1->p + f.c1->grad_p.dot(f.r1);
-            f.p = 0.5 * (p_0 + p_1);
-
-            /// temperature
-            const Scalar T_0 = f.c0->T + f.c0->grad_T.dot(f.r0);
-            const Scalar T_1 = f.c1->T + f.c1->grad_T.dot(f.r1);
-            f.T = f.ksi0 * T_0 + f.ksi1 * T_1;
-
-            /// velocity
-            if (f.U.dot(f.n01) > 0)
-            {
-                const Scalar u_0 = f.c0->U.x() + f.c0->grad_U.col(0).dot(f.r0);
-                const Scalar v_0 = f.c0->U.y() + f.c0->grad_U.col(1).dot(f.r0);
-                const Scalar w_0 = f.c0->U.z() + f.c0->grad_U.col(2).dot(f.r0);
-                f.U = { u_0, v_0, w_0 };
-            }
-            else
-            {
-                const Scalar u_1 = f.c1->U.x() + f.c1->grad_U.col(0).dot(f.r1);
-                const Scalar v_1 = f.c1->U.y() + f.c1->grad_U.col(1).dot(f.r1);
-                const Scalar w_1 = f.c1->U.z() + f.c1->grad_U.col(2).dot(f.r1);
-                f.U = { u_1, v_1, w_1 };
-            }
-
-            /// density
-            if (f.U.dot(f.n01) > 0)
-                f.rho = f.c0->rho + f.c0->grad_rho.dot(f.r0);
-            else
-                f.rho = f.c1->rho + f.c1->grad_rho.dot(f.r1);
-        }
+        } else
+            calcInternalFacePrimitiveValue(f);
 
         /// Conservative variables.
         // f.rhoU = f.rho * f.U;
@@ -317,10 +240,10 @@ void ForwardEuler(Scalar TimeStep)
     calcCellProperty();
 
     /// Gradients at centroid of each cell.
-    calcCellGradient();
+    calc_cell_primitive_gradient();
 
     /// Gradients at centroid of each face.
-    calcFaceGradient();
+    calc_face_primitive_gradient();
 
     /// Interpolate values on each face.
     calcFaceValue();
@@ -342,13 +265,11 @@ void ForwardEuler(Scalar TimeStep)
     for (auto &f : face)
     {
         if (!f.atBdry)
-        {
             f.rhoU_star = f.ksi0 * f.c0->rhoU_star + f.ksi1 * f.c1->rhoU_star;
-        }
     }
 
     /// Correction Step
-    for(int k = 0; k < NOC_ITER; ++k)
+    for (int k = 0; k < NOC_ITER; ++k)
     {
         calcPressureCorrectionEquationRHS(Q_dp_2, TimeStep);
         sx_solver_amg_solve(&dp_solver_2, &x_dp_2, &Q_dp_2);
@@ -357,11 +278,11 @@ void ForwardEuler(Scalar TimeStep)
             auto &c = cell.at(i);
             c.p_prime = sx_vec_get_entry(&x_dp_2, i);
         }
-        calcPressureCorrectionGradient();
-        calcFacePressureCorrectionGradient();
+        calc_cell_pressure_correction_gradient();
+        calc_face_pressure_correction_gradient();
     }
 
-    for(auto &f : face)
+    for (auto &f : face)
         f.rhoU = f.rhoU_star - TimeStep * f.grad_p_prime;
 
     /// Update
