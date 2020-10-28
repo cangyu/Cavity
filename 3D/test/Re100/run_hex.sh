fe319158aa49a0fc2b4f7ffb3aaa3320487e11df
Yu Cang <yhcy1993@gmail.com>
2020-10-28 13:04:28 +0800

    Add execution command for convenience.

---
+++ 3D/test/Re100/run_hex.sh
@@ -0,0 +1,1 @@
+$CAVITY_DIR/bin/CAVITY --mesh 32_translated.txt --Re 100 --time-span 10 --write-interval 200
---
+++ 3D/test/Re100/run_tet.sh
@@ -0,0 +1,2 @@
+$CAVITY_DIR/bin/CAVITY --mesh 75470_translated.txt --Re 100 --time-span 10 --write-interval 200
+
