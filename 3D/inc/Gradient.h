8205a5e52d9469524cd0baa216e61f752a14bfea
Yu Cang <yhcy1993@gmail.com>
2020-12-16 17:08:02 +0800

    Enforce B.C. for all input IC.

--- 3D/inc/Gradient.h
+++ 3D/inc/Gradient.h
@@ -5,25 +5,33 @@ void prepare_lsq();
 void prepare_gg();
 void prepare_TeC_operator();
 
-void GRAD_Cell_Density();
-void GRAD_Cell_Velocity();
-void GRAD_Cell_Pressure();
-void GRAD_Cell_Temperature();
-
+/* Face */
+/// @(n)
 void GRAD_Face_Density();
 void GRAD_Face_Velocity();
 void GRAD_Face_Pressure();
 void GRAD_Face_Temperature();
 
-void GRAD_Cell_Temperature_star();
-void GRAD_Cell_Temperature_next();
-
-Scalar GRAD_Cell_PressureCorrection();
+/// @(*)
 void GRAD_Face_PressureCorrection();
 
-void GRAD_Cell_Velocity_next();
+/// @(m+1)
 void GRAD_Face_Velocity_next();
 
+/* Cell */
+/// @(n)
+void GRAD_Cell_Density();
+void GRAD_Cell_Velocity();
+void GRAD_Cell_Pressure();
+void GRAD_Cell_Temperature();
+
+/// @(*)
+void GRAD_Cell_Temperature_star();
+Scalar GRAD_Cell_PressureCorrection();
+
+/// @(m+1)
+void GRAD_Cell_Velocity_next();
 void GRAD_Cell_Pressure_next();
+void GRAD_Cell_Temperature_next();
 
 #endif
--- 3D/src/IC.cc
+++ 3D/src/IC.cc
@@ -12,30 +12,8 @@ extern NaturalArray<Face> face;
 extern NaturalArray<Cell> cell;
 extern NaturalArray<Patch> patch;
 
-void IC_Zero()
+static void prepare()
 {
-    const Scalar P0 = 101325.0; /// Pa
-    const Scalar T0 = 300.0; /// K
-
-    /// {$\vec{U}$, $p$, $T$} @(n), Cell
-    for (auto &C : cell)
-    {
-        C.U.setZero();
-        C.p = P0;
-        C.T = T0;
-    }
-
-    /// {$\vec{U}$, $p$, $T$} @(n), Face(Internal)
-    for(auto &f : face)
-    {
-        if(!f.at_boundary)
-        {
-            f.U.setZero();
-            f.p = P0;
-            f.T = T0;
-        }
-    }
-
     /// Enforce B.C. for {$\vec{U}$, $p$, $T$} @(n), Face(Boundary)
     BC_Primitive();
 
@@ -96,6 +74,33 @@ void IC_Zero()
     }
 }
 
+void IC_Zero()
+{
+    const Scalar P0 = 101325.0; /// Pa
+    const Scalar T0 = 300.0; /// K
+
+    /// {$\vec{U}$, $p$, $T$} @(n), Cell
+    for (auto &C : cell)
+    {
+        C.U.setZero();
+        C.p = P0;
+        C.T = T0;
+    }
+
+    /// {$\vec{U}$, $p$, $T$} @(n), Face(Internal)
+    for(auto &f : face)
+    {
+        if(!f.at_boundary)
+        {
+            f.U.setZero();
+            f.p = P0;
+            f.T = T0;
+        }
+    }
+
+    prepare();
+}
+
 void IC_File(const std::string &DATA_PATH, int &iter, Scalar &t)
 {
     /// {$\rho$, $\vec{U}$, $p$, $T$} @(n), Cell, Face(Boundary+Internal), Node
@@ -105,39 +110,6 @@ void IC_File(const std::string &DATA_PATH, int &iter, Scalar &t)
     read_data(dts, iter, t);
     dts.close();
 
-    /// {$\nabla \rho$, $\nabla \vec{U}$, $\nabla p$, $\nabla T$} @(n), Cell & Face(Boundary+Internal)
-    GRAD_Cell_Density();
-    GRAD_Cell_Velocity();
-    GRAD_Cell_Pressure();
-    GRAD_Cell_Temperature();
-    GRAD_Face_Density();
-    GRAD_Face_Velocity();
-    GRAD_Face_Pressure();
-    GRAD_Face_Temperature();
-
-    /// Property
-    CALC_Cell_Viscosity();
-    CALC_Cell_Conductivity();
-    CALC_Cell_SpecificHeat();
-    CALC_Face_Viscosity();
-    CALC_Face_Conductivity();
-    CALC_Face_SpecificHeat();
-
-    /// {$\tau$} @(n), Cell & Face(Boundary+Internal)
-    CALC_Cell_ViscousShearStress();
-    CALC_Face_ViscousShearStress();
-
-    /// {$h$, $\rho h$, $\rho \vec{U}$} @(n), Cell & Face(Boundary+Internal)
-    for (auto &C : cell)
-    {
-        C.h = Enthalpy(C.specific_heat_p, C.T);
-        C.rhoh = C.rho * C.h;
-        C.rhoU = C.rho * C.U;
-    }
-    for (auto &f : face)
-    {
-        f.h = Enthalpy(f.specific_heat_p, f.T);
-        f.rhoh = f.rho * f.h;
-        f.rhoU = f.rho * f.U;
-    }
+    /// Boundary values will be updated.
+    prepare();
 }
